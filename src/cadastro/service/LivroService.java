c7e0fb8a47b9b159751a4ad03a0c77483f86cc2b
Sciel Ramos Rodrigues Buitrago <feliperrbuitrago@gmail.com>
2026-03-25 21:59:51 -0300

    adicionado novas validação para LivroService

--- src/cadastro/service/LivroService.java
+++ src/cadastro/service/LivroService.java
@@ -1,6 +1,7 @@
 package cadastro.service;
 
 import cadastro.model.Livro;
+import cadastro.repository.LivroRepository;
 
 import java.util.List;
 
@@ -10,30 +11,47 @@ public class LivroService {
     public LivroService() {
         this.livroRepository = new LivroRepository();
     }
-
-    public LivroService(LivroRepository livroRepository) {
+    public LivroService(LivroRepository livroRepository){
         this.livroRepository = livroRepository;
     }
 
     public Livro cadastrar(Livro livro) {
-        return null;
+        return livroRepository.salvar(livro);
     }
 
     public Livro buscarPorId(int id) {
-        return null;
+        Livro livro = livroRepository.buscarPorId(id);
+
+        if (livro == null) {
+            throw new RuntimeException("Livro não encontrado");
+        }
+        return livro;
     }
 
     public List<Livro> buscarPorNome(String nome) {
-        return null;
+        return livroRepository.buscarPorNome(nome);
     }
 
     public List<Livro> listarTodos() {
-        return null;
+        return livroRepository.listarTodos();
     }
 
     public void alterar(Livro livro) {
+        Livro existe = livroRepository.buscarPorId(livro.getId());
+
+        if(existe == null) {
+            throw new RuntimeException("Livro não encontrado para atualização");
+        }
+        livroRepository.atualizar(livro);
     }
 
     public void remover(int id) {
+        Livro livro = livroRepository.buscarPorId(id);
+
+        if(livro == null){
+            throw new RuntimeException("Livro não encontrado para remoção");
+        }
+        livroRepository.deletar(id);
     }
+
 }
